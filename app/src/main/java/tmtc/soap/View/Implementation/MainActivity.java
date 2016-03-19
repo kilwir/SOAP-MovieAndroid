953fd058a77966cddc5af4b9117a72bde07a44d1
Kilwir <jallanremy@gmail.com>
2016-03-20 00:28:06 +0100

    start implement research

--- app/src/main/java/tmtc/soap/View/Implementation/MainActivity.java
+++ app/src/main/java/tmtc/soap/View/Implementation/MainActivity.java
@@ -1,15 +1,31 @@
 package tmtc.soap.View.Implementation;
 
 import android.app.ActivityOptions;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.NavigationView;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
+
+import com.orhanobut.logger.Logger;
 
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -82,12 +98,31 @@ public class MainActivity extends DrawerAppCompatActivity implements MainView, N
     public void navigateToMovie(View movieView,Movie movie) {
         Intent intent = new Intent(this,MovieActivity.class);
         intent.putExtra("movie", Parcels.wrap(movie));
-        ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,movieView,"poster_movie");
-        startActivity(intent,activityOptions.toBundle());
+        ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, movieView, "poster_movie");
+        startActivity(intent, activityOptions.toBundle());
     }
 
     @Override
     public void ItemMovieListenerOnClick(View view,Movie movie) {
-        this.navigateToMovie(view,movie);
+        this.navigateToMovie(view, movie);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //Set icon color
+        MenuItem item = menu.findItem(R.id.action_search);
+        item.getIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
